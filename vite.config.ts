2733f76dccb87a35db62643de1f9afd2c258504d
abratko <abratko@gotrg.com>
2024-10-02 00:17:45 +0300

    build: add config for tsc, npm, vite

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+    plugins: [
+    dts({ rollupTypes: true })
+],
+    build: {
+        lib: {
+            fileName: 'index',
+            entry: resolve(__dirname, 'src/index.ts'),
+            formats: ['es']
+        },
+        rollupOptions: {
+            external: ["vue-demi"],
+            output: {
+                globals: {
+                    vue: "Vue",
+                },
+            },
+        },
+    }
+})
